f453e478eda734e6d2cd2de0b7c112d906d72765
Kevin <kdaviesnz@gmail.com>
2023-11-09 14:17:09 +1300

    encrytion

---
+++ encryption.py
@@ -0,0 +1,9 @@
+'''
+Perform symetic encryption in order below:
+Find library to provide acess to AES256 algorithm
+Create and save your AED256 key
+Create string to test encryption
+Perform AES256 symetric encryption on the data to get ciphir text.
+Use AES256 symetri decryption to get original text.
+Use assert to check if decrypted data is the same as original text.
+'''
