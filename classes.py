c535b33c43bd89f7f149da26bfdf23e0f7ebef03
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2023-11-10 16:47:59 +1300

    oop

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ Customize the value behaviour of drive:
 Inside ElectricCar class:
     redefine the drive(self, distance) function again.
     it will have the same behaviour regarding 'milage_km"
-    but add at the end some code so that self.range sets subtracted
+    but add at the end some code so that self.range gets subtracted
     by the drive 'distance.'
 Test the drive behaviour of ElectricCar class:
     initialize an object from ElectricCar class "my_ev"
@@ -22,6 +22,42 @@ Test the drive behaviour of IceCAr class:
     notice via print(my_ice_car.__dict__) that the range has been reduce.
    
 '''
+class Car:
+    
+    def __init__(self, brand:str, milage_km:int) -> None:
+        self.brand = brand
+        self.milage_km = milage_km
+
+    def drive(self, distance_km:float):
+        self.milage_km += distance_km
+
+
+class ElectricCar(Car):
+
+    def __init__(self, brand:str, milage_km:int, range:int) -> None:
+        super().__init__(brand, milage_km)
+        self.range = range
+
+    def drive(self, distance_km: float):
+        super().drive(distance_km)
+        self.range -= distance_km
+
+
+
+class Ice(Car):
+    def __init__(self, brand:str, milage_km:int, fuel_consumption:float, fuel_level:float) -> None:
+        Car.__init__(self, brand, milage_km)
+        self.fuel_consumption = fuel_consumption
+        self.fuel_level = fuel_level
+
+my_ev_car = ElectricCar('Tesla', 200, 10)
+my_ev_car.drive(distance_km = 9999)
+
+
+my_ice_car = ElectricCar('V8', 100, 100, 10)
+
+
+
 
 
 
