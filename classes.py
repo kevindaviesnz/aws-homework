d8df042ac52d2b6c1d1bb0a120c6bfbb37ed2c84
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2023-11-11 19:23:30 +1300

    classes.py

--- classes.py
+++ classes.py
@@ -25,11 +25,12 @@ Test the drive behaviour of IceCAr class:
 class Car:
     
     def __init__(self, brand:str, milage_km:int) -> None:
-        assert(milage_km > 0)
+        assert milage_km > 0, "milage_km can't be less than 0"
         self.brand = brand
         self.milage_km = milage_km
 
     def drive(self, distance_km:float):
+        assert distance_km > 0,  f"distance_km must be greater than 0 (got {distance_km})"
         self.milage_km += distance_km
 
 
@@ -37,27 +38,27 @@ class ElectricCar(Car):
 
     def __init__(self, brand:str, milage_km:int, range:int) -> None:
         super().__init__(brand, milage_km)
-        assert(range > 0)
+        assert range > 0, "range must be greater than 0"
         self.range = range
 
     def drive(self, distance_km: float):
         super().drive(distance_km)
         new_range = self.range - distance_km
-        assert(new_range > 0, "Oh no! You've run out of battery!")
-        
+        assert new_range > 0, "Oh no! You've run out of battery!"
+        self.range = new_range
 
 my_ev_car = ElectricCar(brand='tesla', milage_km=200, range=20000)
 my_ev_car.drive(distance_km=100)
-assert(my_ev_car.milage_km == 300)
-assert(my_ev_car.range == 19900)
+assert my_ev_car.milage_km == 300, "Expected milage_km to be 300"
+assert my_ev_car.range == 19900, f"Expected range to be 19900 but got {my_ev_car.range}"
 
 my_ev_car.drive(distance_km=20)
-assert(my_ev_car.milage_km == 320)
-assert(my_ev_car.range == 19880)
+assert my_ev_car.milage_km == 320, "Expected milage_km to be 320"
+assert my_ev_car.range == 19880, "Expected range to be 19880"
 
 my_ev_car.drive(distance_km=200)
-assert(my_ev_car.milage_km == 520)
-assert(my_ev_car.range == 19680)
+assert my_ev_car.milage_km == 520, "Expected milage_km to be 520"
+assert my_ev_car.range == 19680, "Expected range to be 19680"
 
 # For both EV and ICE cars distance_km can't be less than 0 when calling drive()
 my_ev_car.drive(distance_km= -1)
@@ -87,30 +88,30 @@ Test the drive behaviour of IceCAr class:
 class IceCar(Car):
     def __init__(self, brand:str, milage_km:int, fuel_consumption:float, fuel_level:float) -> None:
         Car.__init__(self, brand, milage_km)
-        assert(fuel_consumption > 0)
-        assert(fuel_level > 0)
+        assert fuel_consumption > 0
+        assert fuel_level > 0
         self.fuel_consumption = fuel_consumption
         self.fuel_level = fuel_level
 
     def drive(self, distance_km: float):
         super().drive(distance_km)
         new_fuel_level = self.fuel_level - (distance_km * self.fuel_consumption / 100)
-        assert(new_fuel_level > 0, "Oh no! You've run out of petrol!")
+        assert new_fuel_level > 0, "Oh no! You've run out of petrol!"
         self.fuel_level = new_fuel_level
 
 my_ice_car = IceCar(brand='V8', milage_km=100, fuel_consumption=100, fuel_level=2000)
 
 my_ice_car.drive(distance_km = 50)
-assert(my_ice_car.milage_km == 150)
-assert(my_ice_car.fuel_level == 1950.0) 
+assert my_ice_car.milage_km == 150
+assert my_ice_car.fuel_level == 1950.0
 
 my_ice_car.drive(distance_km = 100)
-assert(my_ice_car.milage_km == 250)
-assert(my_ice_car.fuel_level == 1850.0)
+assert my_ice_car.milage_km == 250
+assert my_ice_car.fuel_level == 1850.0
 
 my_ice_car.drive(distance_km = 200)
-assert(my_ice_car.milage_km == 450)
-assert(my_ice_car.fuel_level == 1650.0)
+assert my_ice_car.milage_km == 450
+assert my_ice_car.fuel_level == 1650.0
 
 # For both EV and ICE cars distance_km can't be less than 0 when calling drive()
 my_ice_car.drive(distance_km= -1)
@@ -160,7 +161,7 @@ class Triangle(Shape):
         # side is not 0 or less and that the sum of the last two sides
         # are not equal to or less than the length of the first side (longest side).
         sides_sorted = sorted([side_a, side_b, side_c])
-        assert(sides_sorted[2] > 0 and sides_sorted[1] + sides_sorted[2] > sides_sorted[0])
+        assert sides_sorted[2] > 0 and sides_sorted[1] + sides_sorted[2] > sides_sorted[0], "Invalid triangle"
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
@@ -176,13 +177,13 @@ class Triangle(Shape):
 # @todo tests
 my_triangle = Triangle(side_a = 20, side_b = 40, side_c = 80)
 #print(my_triangle.area())
-#assert(Triangle.area() == 10)
+#assert Triangle.area() == 10)
 
 class Rectangle(Shape):
 
     def __init__(self, side_a: int, side_b: int) -> int:
         super().__init__()
-        assert(side_a > 0 and side_b > 0 and side_a != side_b)
+        assert side_a > 0 and side_b > 0 and side_a != side_b, "Invalid reactangle"
         self.side_a = side_a
         self.side_b = side_b
 
@@ -191,13 +192,13 @@ class Rectangle(Shape):
 
 # todo tests
 my_reactangle = Rectangle(20, 50)
-assert(my_reactangle.area() == 1000)
+assert my_reactangle.area() == 1000, "Expected area of rectangle to be 1000"
            
 class Circle(Shape):
 
     def __init__(self, radius:int):
         super().__init__()
-        assert(radius > 0)
+        assert radius > 0, "Radius must be greater than 0"
         self.radius = radius
 
     def area(self) -> int:
@@ -207,7 +208,7 @@ class Circle(Shape):
 # @todo tests
 my_circle = Circle(100)
 print(my_circle.area())
-assert(int(my_circle.area()) == 31415)
+assert int(my_circle.area()) == 31415, "Expected area of circle to be 31415"
 
 
 
