1fdb4e79880c2b5fc11bbc3d7626c92ae8aa49ae
Kevin <kdaviesnz@gmail.com>
2023-11-16 11:21:45 +1300

    API

--- API/module/main.py
+++ API/module/main.py
@@ -12,7 +12,8 @@ async def root():
     return {"message":"hello world"}
 
 # Expecting the serial num to be the keys of the dictionar
-my_inventory_items: Dict[str, InventoryItem]
+my_inventory_items: Dict[str, InventoryItem] = {}
+my_inventory_item_dict: Dict[str, InventoryItem] = {}
 
 item_origin = ItemOrigin(country="Ethiopia", production_date="10th Aug 2021")
 
@@ -47,12 +48,20 @@ in the body of the request.
 @app.put("/items/{serial_number}")
 # item is passed by the request body as it is a complex type.
 def update_item(serial_number:str, item:InventoryItem):
+    my_inventory_item_dict[serial_number] = item
+    print(serial_number)
+    print(my_inventory_item_dict)
+
+'''
     existing_item = next((i for i in my_inventory_items if i.serial_number == serial_number), None)
     #existing_item = my_inventory_items.get(serial_number)
     if existing_item:
         my_inventory_items.remove(existing_item)
     my_inventory_items.append(item)
     return item
+'''
+
+
 
 # Run:
 # $ uvicorn main:app --reload
