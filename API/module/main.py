452e3de407a2209ac2d4bd113fffdbe55b6838d8
Kevin <kdaviesnz@gmail.com>
2023-11-16 14:12:02 +1300

    API

--- API/module/main.py
+++ API/module/main.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, field_validator
 from dto import ItemOrigin, InventoryItem
-from typing import Dict
+from typing import Dict, List
 import json
 
 from fastapi import FastAPI, HTTPException
@@ -22,8 +22,18 @@ my_inventory_items = [
   InventoryItem(name="printer", quantity=1, serial_number="H896", origin=item_origin)
 ]
 
+
+
 @app.get("/items/{serial_number}")
-def read_item(serial_number: str):
+def read_item(serial_number: str) -> InventoryItem:
+    if serial_number in my_inventory_item_dict.keys():
+        return my_inventory_item_dict[serial_number]
+    else:
+        raise HTTPException(status_code=404, detail=f"Could not find item with serial number {serial_number}")
+
+
+
+    
     item = next((i for i in my_inventory_items if i.serial_number == serial_number), None)
     if item is None:
         raise HTTPException(status_code=404, detail=f"Could not find item with serial number {serial_number}")
@@ -31,6 +41,11 @@ def read_item(serial_number: str):
     # Convert the item to a dictionary, ensuring nested models are properly converted
     item_dict = json.loads(json.dumps(item.dict()))
     return item_dict
+
+@app.get("/items/")
+def read_items() -> List[InventoryItem]:
+    return my_inventory_item_dict.values()
+
 
 '''
 Requires something like:
@@ -47,7 +62,7 @@ in the body of the request.
 '''
 @app.put("/items/{serial_number}")
 # item is passed by the request body as it is a complex type.
-def update_item(serial_number:str, item:InventoryItem):
+def update_item(serial_number:str, item:InventoryItem) -> None:
     my_inventory_item_dict[serial_number] = item
     print(serial_number)
     print(my_inventory_item_dict)
@@ -60,6 +75,14 @@ def update_item(serial_number:str, item:InventoryItem):
     my_inventory_items.append(item)
     return item
 '''
+
+@app.delete("/items/{serial_number}")
+def delete_item(serial_number:str) -> Dict:
+    if serial_number in my_inventory_item_dict.keys():
+        my_inventory_item_dict.pop(serial_number)
+        return {"message":f"Deleted item {serial_number}"}
+    else:
+        raise HTTPException(status_code=404, detail=f"Could not find item with serial number {serial_number}")
 
 
 
