26338dab1c655b1ff329bf96bb397c5846d672b7
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2023-11-17 16:46:27 +1300

    ...

--- homework_wk3/modules_api_testing/test/test_main.py
+++ homework_wk3/modules_api_testing/test/test_main.py
@@ -2,6 +2,7 @@
 
 from fastapi.testclient import TestClient
 from main import app
+from typing import Final
 
 # Looks for files names with test_*.py
 # $ pytest
@@ -14,17 +15,41 @@ def test_basic_example():
 
 client = TestClient(app)
 
-def test_put_api():
-    response = client.put("/items/T240", json={
+put_request_json: Final = {
         "name": "Product X",
         "quantity": 10,
-        "serial_number": "H897",
+        "serial_number": "T240",
         "origin": {
             "country": "Ethiopia",
             "production_date": "10th Aug 2000"
         }
+    }
+
+def test_put_api():
+
+    response = client.put("/items/T240", json={
+        "name": "Product X",
+        "qty": 10,
+        "serial_number": "H897",
     })
-    assert response.status_code == 200
+    assert response.status_code == 422, f"Expecting 200 but got {response.status_code}"
+
+    response = client.put("/items/T240", json=put_request_json)
+    assert response.status_code == 200, f"Expecting 200 but got {response.status_code}"
+
+def test_get_api():
+
+    response = client.get("/items/T241")
+    assert response.status_code == 404, f"Expecting 404 but got {response.status_code}"
+
+    response = client.get("/items/T240")
+    assert response.status_code == 200, f"Expecting 200 but got {response.status_code}"
+    #assert response.json() == put_request_json
+
+def test_delete_api():
+
+    response = client.delete("/items/T240")
+    assert response.status_code == 404, f"Expecting 404 but got {response.status_code}"
+
+        
     
-
-
