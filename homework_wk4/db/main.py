bd7ef29c7f2ac5283cb09fce74d4ae5d88f46868
Kevin <kdaviesnz@gmail.com>
2023-11-23 11:49:55 +1300

    ...

--- homework_wk4/db/main.py
+++ homework_wk4/db/main.py
@@ -1,19 +1,17 @@
 # pip3 install sqlmodel
 # DAO
-from typing import Optional
+# http://localhost:8000/#docs
+from typing import Optional, List
 from sqlmodel import Field, SQLModel, Session, create_engine, select
 from fastapi import FastAPI, HTTPException
 
-# DAO class -> represents entry in the databasel.
-class Hero(SQLModel, table=True):
+# DAO class -> represents entry in the database.
+class Hero(SQLModel, table=True): # DAO: table-True
     id: Optional[int] = Field(default=None, primary_key=True) # required
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = None
     # team_id: Optional[int] = Field(default=None, foreign_key="team_id")
-
-#hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-#hero_2 = Hero(name="Spider body", secret_name="Pedro")
 
 # init sqlite
 sqlite_filename = "database.db"
@@ -39,19 +37,54 @@ def create_hero(hero:Hero, name:str):
         session.commit()
     yield(f"Created hero {name}")
 
-@app.get("/hero/{name}")
+@app.get("/hero/{name}", response_model=Hero)
 def get_hero(name:str):
     with Session(engine) as session:
         statement = select(Hero).where(Hero.name == name)
         hero = session.exec(statement).first()
+        # Need to add exception handling if hero not found
         return hero
 
-@app.get("/heroes/")
+@app.get("/heroes/", response_model=List[Hero])
 def get_heroes():
     with Session(engine) as session:
         statement = select(Hero)
         heroes = session.exec(statement).all()
         return heroes
+
+# PATCH
+# Define new DTO model specialise class only for doing updates (read, update, create)
+# Defines what info is required.
+class HeroUpdateDTO(SQLModel):
+    name: Optional[str] = None
+    secret_name: Optional[str] = None
+    age: Optional[int] = None
+
+
+'''
+body  - raw  - json
+{
+    "name":"Deadpool",
+    "secret_name": "Thor"
+}
+'''
+@app.patch("/hero", response_model=Hero)
+def change_secret_name(hero_update:HeroUpdateDTO):
+    with Session(engine) as session:
+        statement = session.select(Hero).where(Hero.name == HeroUpdateDTO.name)
+        db_hero = session.exec(statement).first()
+        # to do : exccepton handling
+        hero_data = hero_update.dict(exclude_unset=True)
+        for key, value in hero_data.items():
+            setattr(db_hero, key, value)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+
+
+
+
+
 
 
 # Run:
